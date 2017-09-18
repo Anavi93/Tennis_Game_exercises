788bcb4641411614cb58e0f23a4431e0a49dc11c
Anavi93 <31243189+Anavi93@users.noreply.github.com>
2017-09-18 23:15:46 +0200

    Refining AI
    Changed function for movement of computer controled paddle, so it can predict when ball will bounce and where it will intersect with it's player's control area

--- js/paddle.js
+++ js/paddle.js
@@ -1,19 +1,59 @@
 const PADDLE_HEIGHT=80;
 const PADDLE_THICKNESS=10;
 const PADDLE_DIST_FROM_EDGE=Math.floor(document.getElementById("gameCanvas").width*0.15);
-const PADDLE_SPEED=10;
+const PADDLE_SPEED=6;
 
 var paddle1X=PADDLE_DIST_FROM_EDGE;
 var paddle2X=document.getElementById("gameCanvas").width-PADDLE_DIST_FROM_EDGE;
 var paddle1Y=250;
 var paddle2Y=250;
 
+//global variable for ball interception with player2 area
+var interY=0;
+
 function computerMovement(){
 	var paddle2YCenter=paddle2Y+PADDLE_HEIGHT/2;
-	if(paddle2YCenter<ballY-35)
+	//ball going to player1, computer is repositiong on center
+	if(ballSpeedX<0){
+	//delete previous interseption point	
+	interY=0;	
+	if(paddle2YCenter-canvas.height/2<-6)
 		paddle2Y += PADDLE_SPEED;
-	if(paddle2YCenter>ballY+35)
+	if(paddle2YCenter-canvas.height/2>6)
 		paddle2Y -= PADDLE_SPEED;
+	}
+	//ball going to computer controled paddle
+	if(ballSpeedX>0){
+		//if we don't have interception point calculate new one
+		if(interY==0){
+			var m1=Math.abs(ballSpeedY)/ballSpeedX*1.0;
+			var distanceY;
+			var distanceX=canvas.width-PADDLE_DIST_FROM_EDGE-ballX;
+			if(ballSpeedY>0)
+				distanceY=canvas.height-ballY;
+			if(ballSpeedY<0)
+				distanceY=ballY;
+			var m=distanceY/distanceX;
+			//there won't be more bouncing of the wall
+			if(m>m1)
+				interY=ballY+(distanceX/ballSpeedX)*ballSpeedY;
+			//there will be bouncing
+			else if(m<m1){
+				var k=distanceX/ballSpeedX;
+				var k1=Math.abs(distanceY/ballSpeedY);
+				if(ballSpeedY>0)
+					interY=canvas.height-(k-k1)*ballSpeedY;
+				if(ballSpeedY<0)
+					interY=(k-k1)*(-ballSpeedY);
+			}
+			//randomizing expected interception so the AI can sometimes make a mistake
+			interY=interY-50+Math.random()*100;
+		}
+		if(paddle2YCenter-20<interY && paddle2YCenter<canvas.height)
+			paddle2Y += PADDLE_SPEED;
+		if(paddle2YCenter+20>interY && paddle2YCenter>0)
+			paddle2Y -= PADDLE_SPEED;
+	}
 	
 }
 
